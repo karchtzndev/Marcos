8dfa9f90659d168de0b29454e62f13c77f41b12e
karchtzndev <317672171+karchtzndev@users.noreply.github.com>
2026-08-16 18:33:27 -0300

    teste

--- cliente/sw.js
+++ cliente/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "omarkin-cliente-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE = "omarkin-cliente-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./mascot.jpg"];
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
