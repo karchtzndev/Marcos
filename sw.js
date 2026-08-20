68d7fa848945ebd7d1d31edc2375e8b38ffd1c86
karchtzndev <317672171+karchtzndev@users.noreply.github.com>
2026-08-19 23:12:15 -0300

    v28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "omarkin-cliente-v19";
+const CACHE = "omarkin-cliente-v20";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png", "/mascot.jpg"];
 
 self.addEventListener('install', (e) => {
