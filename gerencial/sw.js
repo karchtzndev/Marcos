e7536f680bf783e45f68e58c275031b1531c2175
karchtzndev <317672171+karchtzndev@users.noreply.github.com>
2026-08-22 13:28:07 -0300

    v33

--- gerencial/sw.js
+++ gerencial/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "omarkin-gerencial-v42";
+const CACHE = "omarkin-gerencial-v44";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./mascot.jpg"];
 
 self.addEventListener('install', (e) => {
